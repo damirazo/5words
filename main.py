e89c2eb07ceac1d609e1e8a50d299e2c33dd4fad
Damir Abdullin <me@damirazo.ru>
2022-05-24 00:55:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+from typing import Iterator
+
+import requests
+
+RUSSIAN_WORDS_TXT = (
+    'https://raw.githubusercontent.com'
+    '/danakt/russian-words/master/russian.txt')
+RUSSIAN_WORDS_PLACE = os.path.join(os.getcwd(), 'russian.txt')
+
+
+def main(pattern: str, excludes: str = '', another_place: str = ''):
+    words = all_russian_words(len(pattern))
+
+    result = []
+    for word in words:
+        if excludes and any((x in excludes for x in word)):
+            continue
+
+        has_char = True
+        for x in another_place:
+            if x not in word:
+                has_char = False
+                break
+
+        if not has_char:
+            continue
+
+        appropriate_word = True
+        for i, char in enumerate(pattern):
+            if char != '*' and char != word[i]:
+                appropriate_word = False
+                continue
+
+        if appropriate_word:
+            result.append(word)
+
+    for row in result:
+        print(row)
+
+
+def all_russian_words(length: int) -> Iterator:
+    rows = None
+    if not os.path.exists(RUSSIAN_WORDS_PLACE):
+        response = requests.get(RUSSIAN_WORDS_TXT)
+        rows = response.content.decode('cp1251')
+
+        with open(RUSSIAN_WORDS_PLACE, 'w+') as f:
+            f.write(rows)
+
+    if rows is None:
+        with open(RUSSIAN_WORDS_PLACE, 'r') as f:
+            rows = f.readlines()
+
+    return (
+        row.lower().strip('\n')
+        for row in rows
+        if len(row.strip('\n')) == length
+        and not row.startswith('-')
+    )
+
+
+def self_test():
+    main('у***а', 'скринго', 'д')
+
+
+self_test()
